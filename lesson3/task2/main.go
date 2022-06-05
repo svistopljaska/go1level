89b160989df6528d9ebc111334fe0655b3c432b8
svistopljaska <svistopljaska@yandex.ru>
2022-06-05 16:50:59 +0500

    3 урок. 2-ое задание - Простые числа до N

---
+++ lesson3/task2/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var numbers []int64
+
+func main() {
+	br := bufio.NewReader(os.Stdin)
+	s, err := br.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	var d int64
+	_, err = fmt.Sscanf(s, "%d", &d)
+	if err != nil {
+		panic(err)
+	}
+
+	var i int64
+	var isSimple bool
+	for i = 2; i < d; i++ {
+		isSimple = true
+		for _, num := range numbers {
+			if i%num == 0 {
+				isSimple = false
+				break
+			}
+		}
+		if isSimple {
+			numbers = append(numbers, i)
+		}
+	}
+
+	fmt.Println(numbers)
+}
