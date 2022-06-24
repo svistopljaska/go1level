2c961ad883417022e2e2f0c5594d7b3d484cc73d
svistopljaska <svistopljaska@yandex.ru>
2022-06-24 20:24:19 +0500

    1

---
+++ sandbox/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	s := "1234567890"
+	b := make([]byte, 3)
+	sr := strings.NewReader(s)
+	for {
+		n, e := sr.Read(b)
+		fmt.Println(n, b, e)
+		if e != nil {
+			break
+		}
+
+	}
+}
